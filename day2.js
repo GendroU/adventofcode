1873d11f87f8f7a80ae0fbe4b80dd4a52e84b057
GendroU <gendroum@gmail.com>
2022-12-05 10:36:18 +0200

    gg

--- day2.js
+++
@@ -1,9 +0,0 @@
-input.slice(0, -1)
-    .split("\n")
-    .reduce((prev, string) => {
-        let sizes = string.split("x"),
-            volume = sizes.reduce((prod, size) => prod * size),
-            perimeter = 2 * (size.reduce((sum, size) => sum - (-size)) - Math.max(...sizes));
-
-        return prev + volume + perimeter;
-    }, 0);
